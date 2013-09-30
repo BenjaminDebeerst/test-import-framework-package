a274915953b42648340c172563fd0a6d2e52612e
Benjamin Debeerst <debeerst@younicos.com>
2013-09-30 17:29:33 +0200

    Initial commit

---
+++ src/main/java/test/SomeCode.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class SomeCode implements BundleActivator
+{
+
+    @Override
+    public void start(BundleContext context) throws Exception
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void stop(BundleContext context) throws Exception
+    {
+        // TODO Auto-generated method stub
+
+    }
+}
